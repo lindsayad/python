7d04304e87c8bdb04df13deba52e77c3a1b4494f
Alex Lindsay <adlinds3@ncsu.edu>
2015-10-15 20:34:28 -0400

    Mostly modifications in python directory, but also some in my cpp directory.

--- salome/2DGeometricProgression.py
+++ salome/2DGeometricProgression.py
@@ -9,13 +9,20 @@ geompy = geomBuilder.New(salome.myStudy)
 from salome.smesh import smeshBuilder
 smesh =  smeshBuilder.New(salome.myStudy)
 
-face1 = geompy.MakeFaceHW(100,100,1)
+p0 = geompy.MakeVertex(0,0,0)
+p1 = geompy.MakeVertex(10,10,0)
+
+# face1 = geompy.MakeFaceHW(100,100,1)
+face1 = geompy.MakeBoxTwoPnt(p0,p1)
 geompy.addToStudy(face1, "face1")
 
 quadra = smesh.Mesh(face1, "Rectangle : quadrangle mesh")
 
-gpAlgo = quadra.Segment()
-gpAlgo.GeometricProgression( 1, 1.2 )
+# gpAlgo = quadra.Segment()
+# gpAlgo.GeometricProgression( 1, 1.2 )
+
+algo1D = quadra.Segment()
+algo1D.NumberOfSegments(10)
 
 # create a quadrangle 2D algorithm for faces
 quadra.Quadrangle()
