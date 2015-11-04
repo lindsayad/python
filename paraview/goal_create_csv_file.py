997e53b0a705f0d2b55234d75ce8f7924c861d8d
Alex Lindsay <adlinds3@ncsu.edu>
2015-11-03 21:17:43 -0500

    Looking at scripting in paraview.

---
+++ paraview/goal_create_csv_file.py
@@ -0,0 +1,12 @@
+from paraview.simple import *
+# cone = Cone()
+# # SetProperties(Resolution=32)
+# shrink = Shrink(cone)
+# show = Show(shrink)
+# render = Render()
+
+cone2 = Cone()
+# SetProperties(Resolution=32)
+shrink2 = Shrink(cone2)
+show2 = Show(shrink2)
+render2 = Render()
